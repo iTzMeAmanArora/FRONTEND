c69fc60f93364d96ec1152396865604fbf9ce784
aman <amansidana55@gmail.com>
2025-12-11 14:00:26 +0530

    html

---
+++ javascript/index.js
@@ -0,0 +1,92 @@
+// let i = prompt("enter your number");
+// do{
+//     if (i ==2) {
+//       console.log("done");
+//       document.writeln("done");
+//     }
+// }
+// while(i!=2){
+// }
+
+// let string="aman"
+// console.log(string.length)
+
+// let string="aman";
+// console.log(string.charAt(2));
+
+
+// let string = "AmAn";
+// let str=" ArOrA"
+// console.log(string+str);
+// console.log(string.concat(str));
+
+// let string = "AmAnArOrA"
+// let str="ArOrA"
+// console.log(string.trim());
+// console.log(string)
+// console.log(str.toUpperCase())
+// console.log(string.toLocaleLowerCase())
+// console.log(string.slice(3,8))
+// console.log(string.slice(-3,8))
+// console.log(string.substring(2,-4))
+// console.log(string.substr(1,3))
+// console.log(string+str);
+// console.log(string.concat(str));
+// console.log(string.replace("A","s"));
+// console.log(string.replaceAll("A", "s"));
+
+// console.log(string.split(""));
+
+
+// let str1="javascript";
+
+// for(let i=0;i<str1.length;i++){
+//     reversestr1=str1[i]+reversestr1;
+// }
+// console.log(reversestr1);
+// for(let i=str1.length-1;i>=0;i--){
+//     reversestr1+=str1[i]
+
+// }
+// console.log(reversestr1);
+// for (let i = 0; i < str1.length; i++) 
+// console.log(str1[0].toUpperCase() + str1.slice(1));
+
+    
+
+
+
+// let a=123;
+// let c=12.556;
+// let b=15;
+// let b=a.toString()
+// console.log(b);
+// console.log(typeof b)
+// console.log(c.toFixed(3))
+// console.log(parseInt(c))
+// console.log(parseFloat(b));
+// console.log(parseFloat(c));
+// console.log(c.toPrecision(2))
+// let b=10/0
+// console.log(b)
+
+// console.log("a"+"b"+ +"1"+"b");
+// console.log(0||1);
+// console.log(!"hello");
+
+
+let string="javascript"
+let count=0;
+for(let i=0;i<string.length; i++){
+if (
+  string[i] == "a" ||
+  string[i] == "e" ||
+  string[i] == "i" ||
+  string[i] == "o" ||
+  string[i] == "u"
+) 
+  count++;
+}
+console.log(count)
+
+
